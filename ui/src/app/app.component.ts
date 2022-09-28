b1361ff5c383d147ed7d6ad12d236c544f1082e5
youchangyouhaoji <youchangyouhaoji@gmail.com>
2022-09-28 15:49:10 -0400

    Worked on local machine

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   title: '';
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: UntypedFormBuilder,
   ) {
   }
 
